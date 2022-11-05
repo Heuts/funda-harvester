655aa08550c44e6d6a8712c082fcd5503e839fa0
Hugo Heuts <hugoheuts@Hugos-MacBook-Pro.local>
2022-11-05 22:27:52 +0100

    Create house objects

--- harvest.py
+++ harvest.py
@@ -1,21 +1,45 @@
+import re
 import urllib3
-import codecs
+from bs4 import BeautifulSoup
+
+from house import House
+
 
 http = urllib3.PoolManager()
 headers = {'user-agent': 'Mozilla/5.0 (Linux; U; Android 8.1.0; ru-ru; Redmi 5 Build/OPM1.171019.026) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141 Mobile Safari/537.36 XiaoMi/MiuiBrowser/11.2.4-g'}
 
-fundaUrl = 'http://www.funda.nl/koop/utrecht/beschikbaar/'
+municipality = 'amsterdam'
+fundaUrl = f'http://www.funda.nl/koop/{municipality}/beschikbaar/'
 
 response = http.request('GET', fundaUrl, headers=headers)
-dataDecoded = response.data.decode('utf-8')
+data = response.data
+dataDecoded = data.decode('utf-8')
 
-print('STATUS: ', response.status, '/n/n')
-print('HEADERS: ', response.headers, '/n/n')
-print('DATA: ', response.data, '/n/n')
-print('DATA DECODED: ', dataDecoded, '/n/n') # Do we need this?
+soup = BeautifulSoup(data, 'html.parser')
+houseSearchResultClass = 'search-result__header'
+priceClass = 'search-result-price'
+houseSearchResultClassTitleClass = 'search-result__header-title'
+houseSearchResultClassSubtitleClass = 'search-result__header-subtitle'
+houseSearchResultDivs = soup.find_all('div', {'class': houseSearchResultClass})
+houseSearchResultClassTitleh2s = soup.find_all(
+    'h2', {'class': houseSearchResultClassTitleClass})
+houseSearchResultClassSubtitle4s = soup.find_all(
+    'h4', {'class': houseSearchResultClassSubtitleClass})
+priceSpans = soup.find_all('span', {'class': priceClass})
+
+for index, div in enumerate(houseSearchResultDivs):
+    href = (div.find('a')['href'])
+    str = href.split('/')[3]
+    lol = str.replace('huis-', '')
+    id = lol.split('-')[0]
+    streetAndHouseNumber = houseSearchResultClassTitleh2s[index].text.strip()
+    postalCodeAndCity = houseSearchResultClassSubtitle4s[index].text
+    splitted = postalCodeAndCity.split()
+    postalCode = splitted[0] + ' ' + splitted[1]
+    city = splitted[2]
+    price = re.findall("([0-9]+[,.]+[0-9]+)", priceSpans[index].text)
 
 
+    house = House(id, streetAndHouseNumber, postalCode, city, price)
+    print(house)
 
-file = codecs.open("utrecht", "w", "utf-8")
-file.write(dataDecoded)
-file.close()
